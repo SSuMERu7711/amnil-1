29c49e3e3353094807b1648686f6f5e19e2c3f7d
HP <HP@DESKTOP-1QT3520>
2022-04-06 18:49:03 +0545

    Add project files.

---
+++ amnil 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace amnil_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo su = new StudentInfo();
+            Console.WriteLine("Full Name:" + "  " + su.LastName + " " + su.LastName);
+            Console.WriteLine("College Name:" + "  " + su.College);
+            Console.WriteLine(" Major Subject:" + "  " + su.MajourSUB);
+
+
+        }
+        public class StudentInfo
+    {
+            public string FirstName = "Sumeru";
+            public string LastName = "Singh";
+            public string College = "Khwopa Engineering College";
+            public string MajourSUB = "C/c++";
+
+
+
+
+        }
+    }
+}
